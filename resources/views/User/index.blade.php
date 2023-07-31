67928cd7a4ff7364cd6bf28eb75f5c8a92ea269a
Anwar <arrowanwar@gmail.com>
2023-07-31 06:56:53 +0600

    'update'

---
+++ resources/views/User/index.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>This is the user page</title>
+</head>
+<body>
+<div class="header">
+
+    <h1>This is header</h1>
+
+    {{$users[0]->email}}
+
+</div>
+<div class="footer">
+
+    <h3 style="color: green">This is footer</h3>
+</div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
+use App\Models\User;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,13 +15,11 @@ use App\Http\Controllers\UserController;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-Route::get ('/home',[HomeController::class, 'index']);
-Route::get ('/user',[UserController::class, 'create']);
-Route::post ('createuser',[UserController::class, 'adduser']);
-Route::get ('/usershow',[UserController::class, 'index']);
-Route::get ('/usershow/{id}',[UserController::class, 'show']);
-Route::get ('/usershow/{id}/delete',[UserController::class, 'delete']);
-Route::get ('/usershow/{id}/edit',[UserController::class, 'edit']);
+ Route::get('/', function () {
+     return view('welcome');
+ });
+
+Route::get('user', function () {
+    $users = User::all();
+    return view('user.index', ['users'=>$users]);
+});
